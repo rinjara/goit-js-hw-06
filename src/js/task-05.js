3a770ccdddbc3d817fb0b6d08127df9d93a48d04
Krystyna Sharhorodska <rinjara@gmail.com>
2022-07-24 21:15:56 +0300

    task 5 done

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -5,3 +5,10 @@
 
 // <input type="text" id="name-input" placeholder="Please enter your name" />
 // <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+
+const nameRef = document.querySelector('#name-output');
+const textRef = document.querySelector('#name-input');
+
+textRef.addEventListener('input', () => {
+  nameRef.textContent = textRef.value ? textRef.value : 'Anonymous';
+});
